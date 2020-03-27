221ce944f7a5fbf836691badcd6775796770ef2b
Timo Reitz <godsboss@gmx.de>
2020-03-27 12:25:26 +0100

    Do not hardcode my repo.

--- golang/godoc-server/build.sh
+++ golang/godoc-server/build.sh
@@ -12,7 +12,13 @@ then
   exit 1
 fi
 
+if [ "${REPOSITORY_NAME}" == '' ]
+then
+  echo "Need to set environmental variable REPOSITORY_NAME."
+  exit 1
+fi
+
 docker build \
   --build-arg GOLANG_VERSION=${GOLANG_VERSION} \
   --build-arg ALPINE_VERSION=${ALPINE_VERSION} \
-  -t godsboss/godoc:golang${GOLANG_VERSION}-alpine${ALPINE_VERSION} .
+  -t ${REPOSITORY_NAME}:golang${GOLANG_VERSION}-alpine${ALPINE_VERSION} .
